ec3755a02ff53967283c2c6c80c9b6d30c56c0c8
Mei <mmullengracia@gmail.com>
2024-11-10 15:55:37 +0100

    Exercici 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,27 @@ import Card from './components/Card';
 import React, { useState } from 'react';
 
 function App() {
-  // Passos a l'usurari
+  // Passos a l'usuari
   const tutorialData = [
-    { title: "Dedica moltes hores", description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament." }
+    { title: "Dedica moltes hores", description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament." },
+    { title: "Programa projectes propis", description: "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge" },
+    { title: "Procura descansar", description: "Descansar bé i desconectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge." }
   ]
   const [step, setStep] = useState(0)
+
+  // Implementa funció nextStep
+  const nextStep = () => {
+    if (step < tutorialData.length - 1) {
+      setStep(prev => prev + 1)
+    }
+  }
 
   return (
     <div>
       <Card
         title={tutorialData[step].title}
         description={tutorialData[step].description}
+        nextStep={nextStep}
       />
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Card({ title, description }) {
+function Card({ title, description, nextStep }) {
     return (
         <div>
             <h2>{title}</h2>
             <p>{description}</p>
+            <button onClick={nextStep}>Següent</button>
         </div>
     )
 }
